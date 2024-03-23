f2b57efd588b58587387039b761e2973092b2812
AlaaAlJebbeh <alaa.jeebeh@gmail.com>
2024-03-23 11:35:28 +0300

    editting names of the javascripe

--- Notifications/logInTest/script.js
+++ Notifications/logInTest/script.js
@@ -1,8 +1,8 @@
 const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
+const learnMoreBtn = document.getElementById('learnMore');
 const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
+learnMoreBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
 

0e7899f2f9b75f8ddef709bc17f1253315b4bea6
AlaaAlJebbeh <alaa.jeebeh@gmail.com>
2024-05-01 15:34:46 +0300

    backEnd Starting1

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import pg from "pg";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-
-
 
 app.use(express.static("public"));
 
@@ -18,4 +15,4 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-  });
+});

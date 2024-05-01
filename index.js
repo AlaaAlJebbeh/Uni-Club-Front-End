dd4321642857f8c615519fb413073f6445459b64
M7mmedHomsi <m7mmedhomsi@gmail.com>
2024-05-01 17:25:46 +0400

    fixes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import path  from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 8000;
 
+app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -14,8 +16,21 @@ app.get("/", (req, res) => {
 
 app.get("/myclubpage", (req, res) => {
 
-    res.render("myclubpage.ejs");
+    const data = {
+        pageTitle: 'My Club Page',
+        message: 'Welcome to my club page!'
+        // Add more data as needed
+    };
+
+    res.render('myclubpage.ejs', data); 
+
 });
+
+app.get("/createclub", (req, res) => {
+
+    res.render("on_click_create_club.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });

7b1a5a9e0ffaca98b62461435e84ad28423448f1
M7mmedHomsi <m7mmedhomsi@gmail.com>
2024-05-01 17:53:53 +0400

    downloaded mysql and established a connection

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path  from "path";
+import mysql from "mysql";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 8000;
+
+const connection = mysql.createConnection({
+host: "localhost",
+user: "root",
+password: "",
+database: "clm",
+});
+
+connection.connect((err) => {
+if (err) {
+    console.error("cant connect to the database", err);
+    return;
+}
+console.log("connected successfully to the database");
+});
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use(express.static("public"));
@@ -33,4 +49,10 @@ app.get("/createclub", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+});
+
+app.get("/comparing", (req, res) => {
+
+    res.render("Compare.ejs");
+
 });

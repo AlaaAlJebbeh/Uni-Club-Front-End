c4a4047ca67ef9f5317c113dda308027552e4863
AlaaAlJebbeh <alaa.jeebeh@gmail.com>
2024-05-08 17:37:11 +0300

    notification with working read

---
+++ public/scripts/notifications.js
@@ -0,0 +1,41 @@
+const unReadMessages = document.querySelectorAll('.unread');
+const unReadMessagesCount = document.getElementById('num-of-notif');
+const markAll = document.getElementById('mark-as-read');
+
+unReadMessagesCount.innerText = unReadMessages.length;
+
+unReadMessages.forEach((message) => {
+    message.addEventListener('click', () => {
+        message.classList.remove('unread');
+        const newUnreadMessages = document.querySelectorAll('.unread');
+        unReadMessagesCount.innerText = newUnreadMessages.length;
+
+        unReadMessages.forEach((button) => {
+            button.addEventListener('click', () => {
+                const form = document.getElementById('readNotificationForm');
+                if (form) {
+                    form.submit();
+                }
+            });
+        });
+    });
+});
+
+// Mark as All read part
+markAll.addEventListener('click', () => {
+    unReadMessages.forEach((message) => {
+        message.classList.remove('unread');
+    });
+    const newUnreadMessages = document.querySelectorAll('.unread');
+    unReadMessagesCount.innerHTML = newUnreadMessages.length;
+});
+
+$(document).ready(function() {
+    // Add click event listener to all buttons with the class 'post-button'
+    $('.read-notification').click(function() {
+        // Retrieve the post ID from the button's ID
+        var eventToShareId = parseInt($(this).attr('id').split('_')[1]);
+        console.log('Button clicked for eventToShareId ID:', eventToShareId);
+        // Perform further actions if needed
+    });
+})

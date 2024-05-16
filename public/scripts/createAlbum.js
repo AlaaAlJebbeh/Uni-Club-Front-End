52b3643fb4c047420db439327a0ddc7165dd3450
aziza <urinboevaaziza3@gmail.com>
2024-05-16 14:44:55 +0300

    upload  album is not working

---
+++ public/scripts/createAlbum.js
@@ -0,0 +1,9 @@
+
+document.getElementById("submitBtn").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent default form submission
+
+  // Perform any client-side validation if necessary
+
+  // Submit the form using JavaScript
+  document.getElementById("createAlbum").submit();
+});
